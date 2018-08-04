db0756ca0c605be22b6da2671a712582b6ecc4f8
Saurin Shah <sourin.shah@gmail.com>
2018-08-04 01:40:15 -0400

    Project created, pom.xml, application.properties updated

---
+++ src/main/java/com/project/emp_classrooms/EmpowerClassroomsApplication.java
@@ -0,0 +1,12 @@
+package com.project.emp_classrooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpowerClassroomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpowerClassroomsApplication.class, args);
+	}
+}
