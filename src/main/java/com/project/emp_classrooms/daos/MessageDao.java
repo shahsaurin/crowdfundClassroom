bb15b63ea74ec0b13b33d1b1cd5bed77243f2fca
Saurin Shah <sourin.shah@gmail.com>
2018-08-19 03:27:44 -0400

    No code change

--- src/main/java/com/project/emp_classrooms/daos/MessageDao.java
+++ src/main/java/com/project/emp_classrooms/daos/MessageDao.java
@@ -22,82 +22,78 @@ public class MessageDao {
 
 	@Autowired
 	MessageRepository messageRepository;
-	
+
 	@Autowired
 	PersonRepository personRepository;
-	
-	
+
 	public void sendMessage(int senderId, int recipientId, Message message) {
 		Person sender = personRepository.findById(senderId).get();
 		Person recipient = personRepository.findById(recipientId).get();
-		
+
 		message.setSender(sender);
 		message.setRecipient(recipient);
-		
-		if(sender instanceof Donor || recipient instanceof Donor) {
+
+		if (sender instanceof Donor || recipient instanceof Donor) {
 			message.setMessageCategory(User.Donor);
 		} else {
 			message.setMessageCategory(User.Teacher);
 		}
-		
+
 		sender.getMessagesSent().add(message);
 		recipient.getMessagesReceived().add(message);
-		
+
 		messageRepository.save(message);
 		personRepository.save(sender);
 		personRepository.save(recipient);
 	}
-	
-	
-	public List<Message> findReceivedMessagesForPerson(int personId) {		
+
+	public List<Message> findReceivedMessagesForPerson(int personId) {
 		List<Message> allMessageInDb = (List<Message>) messageRepository.findAll();
 		List<Message> receivedMessages = new ArrayList<Message>();
-		
+
 		for (Iterator<Message> iterator = allMessageInDb.iterator(); iterator.hasNext();) {
 			Message message = (Message) iterator.next();
 			System.out.println(message);
-			if(message.getRecipient() != null && message.getRecipient().getId() == personId) {
+			if (message.getRecipient() != null && message.getRecipient().getId() == personId) {
 				receivedMessages.add(message);
 			}
 		}
 		return receivedMessages;
 	}
-	
-	
+
 	public List<Message> findSentMessagesForPerson(int personId) {
 		List<Message> allMessageInDb = (List<Message>) messageRepository.findAll();
 		List<Message> sentMessages = new ArrayList<Message>();
-		
+
 		for (Iterator<Message> iterator = allMessageInDb.iterator(); iterator.hasNext();) {
 			Message message = (Message) iterator.next();
-			if(message.getSender() != null && message.getSender().getId() == personId) {
+			if (message.getSender() != null && message.getSender().getId() == personId) {
 				sentMessages.add(message);
 			}
 		}
 		return sentMessages;
 	}
-	
-	
-//	'createMessage' is not used for the app:
+
+	// 'createMessage' is not used for the app:
 	public Message createMessage(Message message) {
 		return messageRepository.save(message);
 	}
-	
+
 	public List<Message> findAllMessages() {
 		return (List<Message>) messageRepository.findAll();
 	}
-	
+
 	public Message findMessageById(int messageId) {
 		Optional<Message> optMessage = messageRepository.findById(messageId);
-		if(optMessage.isPresent()) {
+		if (optMessage.isPresent()) {
 			return optMessage.get();
 		}
 		return null;
 	}
-	
+
 	public Message updateMessage(int id, Message updatedMessage) {
 		Optional<Message> optMessage = messageRepository.findById(id);
-		if(optMessage.isPresent()) {
+		if (optMessage.isPresent()) {
 			Message oldMessage = optMessage.get();
 			oldMessage.setDescription(updatedMessage.getDescription());
 			oldMessage.setLastUpdated(new Date(Calendar.getInstance().getTimeInMillis()));
@@ -105,30 +101,27 @@ public class MessageDao {
 		}
 		return null;
 	}
-	
+
 	public void deleteMessageById(int id) {
 		messageRepository.deleteById(id);
 	}
-	
+
 	public void deleteAllMessages() {
 		messageRepository.deleteAll();
 	}
-	
-	
-	
+
 	public void test() {
-//		Delete all messages:
-//		deleteAllMessages();
-//		
-//		Message d1 = new Message();
-//		d1.setDescription("Sample_message 1");
-//		d1.setMessageCategory(User.Donor);
-//		
-//		Message d2 = createMessage(d1);
-//		
-//		d1.setDescription("edited_description 1");
-//		updateMessage(d2.getId(), d1);
-		
-		
+		// Delete all messages:
+		// deleteAllMessages();
+		//
+		// Message d1 = new Message();
+		// d1.setDescription("Sample_message 1");
+		// d1.setMessageCategory(User.Donor);
+		//
+		// Message d2 = createMessage(d1);
+		//
+		// d1.setDescription("edited_description 1");
+		// updateMessage(d2.getId(), d1);
+
 	}
 }
